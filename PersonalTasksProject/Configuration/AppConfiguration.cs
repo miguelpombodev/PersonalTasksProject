fcf1e678f9ae1b2dec2e5d67a09cd4b298ce6923
Miguel Pombo <miguel.pomboshape@modec.com>
2025-03-06 11:29:00 -0300

    feat: add serilog into project

--- PersonalTasksProject/Configuration/AppConfiguration.cs
+++ PersonalTasksProject/Configuration/AppConfiguration.cs
@@ -1,11 +1,26 @@
+using Amazon;
+using Amazon.Runtime;
+using Amazon.S3;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using PersonalTasksProject.Business.Implementations;
+using PersonalTasksProject.Business.Interfaces;
 using PersonalTasksProject.Configuration.Settings;
+using PersonalTasksProject.Context;
+using PersonalTasksProject.DTOs.Mappings;
+using PersonalTasksProject.Providers;
+using PersonalTasksProject.Providers.Interfaces;
+using PersonalTasksProject.Repositories.Implementations;
+using PersonalTasksProject.Repositories.Interfaces;
+using Serilog;
 
 namespace PersonalTasksProject.Configuration;
 
 public class AppConfiguration : IAppConfiguration
 {
     private static AppConfiguration _instance;
-    
+
     private static readonly object _lock = new object();
 
     public static AppConfiguration Instance
@@ -25,8 +40,137 @@ public class AppConfiguration : IAppConfiguration
     }
 
     public string PostgresConnection { get; set; }
-    public SMTPConfigurationSettings Smtp { get; set; } = new ();
-    public JwtConfigurationSettings Jwt { get; set; }= new ();
-    public AwsConfigurationSettings Aws { get; set; }= new ();
+    public SMTPConfigurationSettings Smtp { get; set; } = new();
+    public JwtConfigurationSettings Jwt { get; set; } = new();
+    public AwsConfigurationSettings Aws { get; set; } = new();
     public Dictionary<string, string> EmailBodies { get; set; }
+
+    private static string GetAndValidateConfigurationValue(WebApplicationBuilder builder, string key,
+        bool isConnectionString = false)
+    {
+        var value = isConnectionString ? builder.Configuration.GetConnectionString(key) : builder.Configuration[key];
+
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            throw new NullReferenceException($"Please provide a proper value to - {key}");
+        }
+
+        return value;
+    }
+
+    private static IConfigurationSection GetAndValidateConfigurationSection(WebApplicationBuilder builder, string key)
+    {
+        var section = builder.Configuration.GetSection(key);
+
+        foreach (var value in section.GetChildren().ToList())
+        {
+            if (string.IsNullOrWhiteSpace(value.Value))
+            {
+                throw new NullReferenceException($"Please provide a proper value to - {value.Key} in {key}");
+            }
+        }
+
+        return section;
+    }
+
+    public static void BuildAppConfigurations(WebApplicationBuilder builder)
+    {
+        var appConfig = AppConfiguration.Instance;
+
+        appConfig.PostgresConnection = GetAndValidateConfigurationValue(builder, "MainDatabaseCS", true);
+        var smtp = new SMTPConfigurationSettings();
+        GetAndValidateConfigurationSection(builder, "SmtpMailTrap").Bind(smtp);
+        appConfig.Smtp = smtp;
+
+        var jwt = new JwtConfigurationSettings();
+        GetAndValidateConfigurationSection(builder, "Jwt").Bind(jwt);
+        appConfig.Jwt = jwt;
+
+        var aws = new AwsConfigurationSettings();
+        GetAndValidateConfigurationSection(builder, "AWSCredentials").Bind(aws);
+        appConfig.Aws = aws;
+
+        appConfig.EmailBodies.Add("Create Task Email", builder.Configuration.GetValue<string>("CreatedTaskEmailBody"));
+
+        builder.Services.AddDbContext<AppDbContext>(
+            options => options.UseNpgsql(
+                appConfig.PostgresConnection
+            )
+        );
+        builder.Services.AddSingleton(appConfig);
+    }
+
+    public static void BuildAppLogger(WebApplicationBuilder builder)
+    {
+        builder.Host.UseSerilog((hostContext, services, configuration) =>
+        {
+            configuration.WriteTo.Console().CreateLogger();
+        });
+    }
+
+    public static void BuildAppServices(WebApplicationBuilder builder)
+    {
+        builder.Services.AddScoped<DbContext>(provider =>
+            provider.GetRequiredService<AppDbContext>());
+
+        builder.Services.AddScoped<IUserService, UserService>();
+        builder.Services.AddScoped<ITasksService, TasksService>();
+        builder.Services.AddScoped<IUserRepository, UserRepository>();
+        builder.Services.AddScoped<ITasksRepository, TasksRepository>();
+        builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        builder.Services.AddAutoMapper(typeof(CreateUserDtoMappingProfile));
+        builder.Services.AddAutoMapper(typeof(CreateTaskDtoMappingProfile));
+
+        builder.Services.AddSingleton<ITokenProvider, TokenProvider>();
+        builder.Services.AddSingleton<SmtpEmailProvider>();
+        builder.Services.AddSingleton<FileProvider>();
+    }
+
+    public static void BuildSwaggerService(WebApplicationBuilder builder)
+    {
+        builder.Services.AddSwaggerGen(o =>
+        {
+            var securityScheme = new OpenApiSecurityScheme
+            {
+                Name = "JWT Authentication",
+                Description = "Enter your JWT Token in this field",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+                Scheme = JwtBearerDefaults.AuthenticationScheme,
+                BearerFormat = "JWT",
+            };
+
+            var securityRequirement = new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = JwtBearerDefaults.AuthenticationScheme
+                        }
+                    },
+                    []
+                }
+            };
+
+            o.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, securityScheme);
+            o.AddSecurityRequirement(securityRequirement);
+        });
+    }
+
+    public static void BuildAwsServices(WebApplicationBuilder builder)
+    {
+        var appConfig = AppConfiguration.Instance;
+        var credentials = new BasicAWSCredentials(appConfig.Aws.AwsAccessKeyId, appConfig.Aws.AwsSecretAccessKey);
+
+        var s3Config = new AmazonS3Config()
+        {
+            RegionEndpoint = RegionEndpoint.SAEast1
+        };
+
+        builder.Services.AddSingleton<IAmazonS3>(new AmazonS3Client(credentials, s3Config));
+    }
 }
--- PersonalTasksProject/Program.cs
+++ PersonalTasksProject/Program.cs
@@ -1,9 +1,7 @@
 using System.Configuration;
 using System.Text;
 using System.Text.Json.Serialization;
-using Amazon;
-using Amazon.Runtime;
-using Amazon.S3;
+
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -14,24 +12,25 @@ using PersonalTasksProject.Configuration;
 using PersonalTasksProject.Configuration.Settings;
 using PersonalTasksProject.Context;
 using PersonalTasksProject.DTOs.Mappings;
-using PersonalTasksProject.Entities;
 using PersonalTasksProject.Extensions;
 using PersonalTasksProject.Middlewares;
 using PersonalTasksProject.Providers;
 using PersonalTasksProject.Providers.Interfaces;
 using PersonalTasksProject.Repositories.Implementations;
 using PersonalTasksProject.Repositories.Interfaces;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-BuildAppConfigurations();
+AppConfiguration.BuildAppLogger(builder);
+AppConfiguration.BuildAppConfigurations(builder);
 
 builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 builder.Services.AddEndpointsApiExplorer();
 
-BuildSwaggerService(builder);
+AppConfiguration.BuildSwaggerService(builder);
 
-BuildAppServices(builder);
+AppConfiguration.BuildAppServices(builder);
 
 builder.Services.AddExceptionHandler<GlobalExceptionHandlerMiddleware>();
 builder.Services.AddProblemDetails();
@@ -65,7 +64,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         }
     );
 
-BuildAwsServices();
+AppConfiguration.BuildAwsServices(builder);
 
 var app = builder.Build();
 
@@ -90,125 +89,3 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.Run();
-
-string GetAndValidateConfigurationValue(string key, bool isConnectionString = false)
-{
-    var value = isConnectionString ? builder.Configuration.GetConnectionString(key) : builder.Configuration[key];
-        
-    if (string.IsNullOrWhiteSpace(value))
-    {
-        throw new NullReferenceException($"Please provide a proper value to - {key}");
-    }
-        
-    return value;
-}
-
-IConfigurationSection GetAndValidateConfigurationSection(string key)
-{
-    var section = builder.Configuration.GetSection(key);
-    
-    foreach (var value in section.GetChildren().ToList())
-    {
-        if (string.IsNullOrWhiteSpace(value.Value))
-        {
-            throw new NullReferenceException($"Please provide a proper value to - {value.Key} in {key}");
-        }
-    }
-    
-    return section;
-}
-
-void BuildAppConfigurations()
-{
-    var appConfig = AppConfiguration.Instance;
-    
-    appConfig.PostgresConnection = GetAndValidateConfigurationValue("MainDatabaseCS", true);
-    var smtp = new SMTPConfigurationSettings();
-    GetAndValidateConfigurationSection("SmtpMailTrap").Bind(smtp);
-    appConfig.Smtp = smtp;
-
-    var jwt = new JwtConfigurationSettings();
-    GetAndValidateConfigurationSection("Jwt").Bind(jwt);
-    appConfig.Jwt = jwt;
-
-    var aws = new AwsConfigurationSettings();
-    GetAndValidateConfigurationSection("AWSCredentials").Bind(aws);
-    appConfig.Aws = aws;
-    
-    appConfig.EmailBodies.Add("Create Task Email", builder.Configuration.GetValue<string>("CreatedTaskEmailBody"));
-    
-    builder.Services.AddDbContext<AppDbContext>(
-        options => options.UseNpgsql(
-            appConfig.PostgresConnection
-        )
-    );
-    builder.Services.AddSingleton(appConfig);
-}
-
-void BuildAppServices(WebApplicationBuilder builder)
-{
-    
-    builder.Services.AddScoped<DbContext>(provider => 
-        provider.GetRequiredService<AppDbContext>());
-
-    builder.Services.AddScoped<IUserService, UserService>();
-    builder.Services.AddScoped<ITasksService, TasksService>();
-    builder.Services.AddScoped<IUserRepository, UserRepository>();
-    builder.Services.AddScoped<ITasksRepository, TasksRepository>();
-    builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
-    builder.Services.AddAutoMapper(typeof(CreateUserDtoMappingProfile));
-    builder.Services.AddAutoMapper(typeof(CreateTaskDtoMappingProfile));
-
-    builder.Services.AddSingleton<ITokenProvider, TokenProvider>();
-    builder.Services.AddSingleton<SmtpEmailProvider>();
-    builder.Services.AddSingleton<FileProvider>();
-}
-
-void BuildSwaggerService(WebApplicationBuilder builder)
-{
-    builder.Services.AddSwaggerGen(o =>
-    {
-        var securityScheme = new OpenApiSecurityScheme
-        {
-            Name = "JWT Authentication",
-            Description = "Enter your JWT Token in this field",
-            In = ParameterLocation.Header,
-            Type = SecuritySchemeType.Http,
-            Scheme = JwtBearerDefaults.AuthenticationScheme,
-            BearerFormat = "JWT",
-        };
-    
-        var securityRequirement = new OpenApiSecurityRequirement
-        {
-            {
-                new OpenApiSecurityScheme
-                {
-                    Reference = new OpenApiReference
-                    {
-                        Type = ReferenceType.SecurityScheme,
-                        Id = JwtBearerDefaults.AuthenticationScheme
-                    }
-                },
-                []
-            }
-        };
-    
-        o.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, securityScheme);
-        o.AddSecurityRequirement(securityRequirement);
-    });
-    
-}
-
-void BuildAwsServices()
-{
-    var appConfig = AppConfiguration.Instance;
-    var credentials = new BasicAWSCredentials(appConfig.Aws.AwsAccessKeyId, appConfig.Aws.AwsSecretAccessKey);
-
-    var s3Config = new AmazonS3Config()
-    {
-        RegionEndpoint = RegionEndpoint.SAEast1
-    };
-
-    builder.Services.AddSingleton<IAmazonS3>(new AmazonS3Client(credentials, s3Config));
-}
