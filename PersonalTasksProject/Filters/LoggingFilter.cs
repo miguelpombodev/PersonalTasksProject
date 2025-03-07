dee3dbb8fc1f97421f2d2639da8bced5a8ff6283
Miguel Pombo <miguel.pomboshape@modec.com>
2025-03-07 17:40:52 -0300

    feat: add grainy request information in logging filter

--- PersonalTasksProject/Filters/LoggingFilter.cs
+++ PersonalTasksProject/Filters/LoggingFilter.cs
@@ -5,7 +5,6 @@ namespace PersonalTasksProject.Filters;
 public class LoggingFilter : IAsyncActionFilter
 {
     private readonly ILogger<LoggingFilter> _logger;
-
     public LoggingFilter(ILogger<LoggingFilter> logger)
     {
         _logger = logger;
@@ -13,7 +12,11 @@ public class LoggingFilter : IAsyncActionFilter
     
     public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
     {
-        _logger.LogInformation("Executing action {action}", context.ActionDescriptor.DisplayName);
+        var httpContext = context.HttpContext;
+        var method = httpContext.Request.Method;
+        var endpointRoute = httpContext.GetEndpoint().Metadata.FirstOrDefault(item => item.ToString().Contains("Route:")).ToString().Split("Route: ")[1].ToUpper();
+
+        _logger.LogInformation($"Executing Endpoint: [{method}] {endpointRoute}");
         await next();
     }
 }
